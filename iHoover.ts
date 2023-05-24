03efea5023d5653499ccc99c29ad8ec9820e946b
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-05-24 17:12:28 +0200

    feat: iHoover class w/ its methods

--- iHoover.ts
+++ iHoover.ts
@@ -32,3 +32,111 @@ export class Room {
 }
 
 
+export enum Orientation {
+    N = "N",
+    S = "S",
+    E = "E",
+    W = "W"
+};
+
+export class iHoover extends Room {
+    private coord_x: number;
+    private coord_y: number;
+    // private instructions: string;
+    private orientation: Orientation;
+    constructor(room_x: number, room_y: number, robot_x: number, robot_y: number, orientation: Orientation) {
+        super(room_x, room_y);
+        this.coord_x = robot_x;
+        this.coord_y = robot_y;
+        this.orientation = orientation;
+        this.setInitPosition(robot_x, robot_y);
+    }
+
+    private setInitPosition(init_x: number, init_y: number) {
+        try {
+        this.checkPosition()
+        const roomDimensions = this.getRoomDimensions()
+        this.getGrid()[roomDimensions.y - 1 - this.coord_y][this.coord_x] = true;
+        console.log(this.getGrid().join('\r\n'));
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    getCurrentPosition() {
+        return {
+            x: this.coord_x,
+            y: this.coord_y,
+            orientation: this.orientation
+        }
+    }
+
+    turnLeft() {
+        switch (this.orientation) {
+            case 'N':
+                this.orientation = Orientation.W
+                break;
+            case 'S':
+                this.orientation = Orientation.E
+                break;
+            case 'E':
+                this.orientation = Orientation.N
+                break;
+            case 'W': 
+                this.orientation= Orientation.S
+                break
+        }
+    }
+
+    turnRight() {
+        switch (this.orientation) {
+            case 'N':
+                this.orientation = Orientation.E
+                break;
+            case 'S':
+                this.orientation = Orientation.W
+                break;
+            case 'E':
+                this.orientation = Orientation.S
+                break;
+            case 'W': 
+                this.orientation= Orientation.N
+                break
+        }
+
+    }
+
+    checkPosition(): boolean {
+        if ((this.coord_x > (this.getRoomDimensions().x - 1)) 
+            || (this.coord_x < 0) 
+            || (this.coord_y < 0) 
+            || this.coord_y > (this.getRoomDimensions().y - 1)) 
+        {
+            throw new Error("iHoover cannot atteign this position in the room.");
+        }
+
+        return true;
+    }
+
+    moveForward() {
+        this.getGrid()[this.getRoomDimensions().y - 1 - this.coord_y][this.coord_x] = false;
+
+        switch(this.orientation) {
+            case "N":
+                this.coord_y += 1;
+                break;
+            case "S":
+                this.coord_y -= 1;
+                break;
+
+            case "E":
+                this.coord_x += 1;
+                break;
+            case "W":
+                this.coord_x -= 1;
+        }
+
+        
+    }
+
+}

ec80b0cb3a15b4406d05ffe3c6d8830ad414cab8
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-05-24 18:59:06 +0200

    feat: program tests

---
+++ Controller.test.ts
@@ -0,0 +1,25 @@
+import { describe, expect, it } from 'vitest'
+import { Controller } from './Controller';
+import { Orientation } from './iHoover';
+
+describe ('Program initalization', () => {
+    it('sets up the robot in the room', () => {
+        const program = new Controller(2, 2, 0, 0, Orientation.N);
+
+        expect(program.getRobotPosition()).toEqual({x: 0, y: 0, orientation: Orientation.N});
+    }) 
+
+    it('checks if the robot is placed within the room', () => {
+        const program = new Controller(2, 2, 3, 3, Orientation.N);
+        
+        expect(program).toThrowError(Error);
+    })
+
+    it('reads the instructions and send the robot on its way', () => {
+        const program = new Controller(2, 2, 0, 0, Orientation.N);
+        program.setInstructions('DA');
+        program.launch();
+
+        expect(program.getRobotPosition()).toEqual({x: 1, y: 0, orientation: Orientation.E})
+    })
+});

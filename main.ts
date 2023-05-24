1a7fc77cd4445795595d0da952d4506b59729eaa
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-05-24 20:04:34 +0200

    feat: input getter function w/ validators

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import { Orientation } from "./iHoover";
 
 interface UserInputs {
     roomLength: number;
-    roomWWidth: number;
+    roomWidth: number;
     robotPosX: number;
     robotPosY: number;
     robotOrientation: Orientation;
@@ -39,3 +39,48 @@ class InputPrompter {
         this.rl.close();
     }
 }
+
+async function getInputs(): Promise<UserInputs | null> {
+    const prompter = new InputPrompter();
+
+    try {
+        const roomLength = await prompter.prompt('Enter room\'s length: ');
+        if (!NumberInputValidator.validateNumber(roomLength)) {
+            throw new Error('Invalid room length. Must be a number.')
+        }
+
+        const roomWidth = await prompter.prompt('Enter room\'s width: ');
+        if (!NumberInputValidator.validateNumber(roomWidth)) {
+            throw new Error('Invalid room width. Must be a number.')
+        }
+
+        const robotPosX = await prompter.prompt('Enter robot\'s initial X position: ');
+        if (!NumberInputValidator.validateNumber(robotPosX)) {
+            throw new Error('Invalid robot\'s initial X. Must be a number.')
+        }
+
+        const robotPosY = await prompter.prompt('Enter robot\'s initial Y: ');
+        if (!NumberInputValidator.validateNumber(robotPosY)) {
+            throw new Error('Invalid robot\'s initial Y. Must be a number.')
+        }
+
+        const robotOrientation = await prompter.prompt('Enter robot\ès inital orientation [N,E,W,S]: ') as Orientation;
+        if (Object.values(Orientation).includes(robotOrientation)) {
+            throw new Error('Invalid Orientation. Must one of N, E, W, S')
+        };
+
+        return {
+            roomLength: parseInt(roomLength),
+            roomWidth: parseInt(roomWidth),
+            robotPosX: parseInt(robotPosX),
+            robotPosY: parseInt(robotPosY),
+            robotOrientation: robotOrientation
+        }
+    } catch(e) {
+        console.error(e)
+        return null
+    } finally {
+        prompter.close();
+    }
+}
+
